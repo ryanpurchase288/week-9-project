109c29766242e4297fffb1c289be7e3e061c46e6
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-02 15:07:07 +0000

    pytest

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -3,7 +3,7 @@ sudo apt update
 yes | sudo apt install python3 python3-pip python3-venv
 python3 -m venv venv
 . venv/bin/activate
-
+pip3 install -r requirements.txt
 cd app1
 python3 -m pytest --cov ./application
 cd ..
