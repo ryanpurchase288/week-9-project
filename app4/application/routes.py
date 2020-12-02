566967386032a37f33825815eb4b0f93f8461bdc
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-02 12:23:57 +0000

    fixes

--- app4/application/routes.py
+++ app4/application/routes.py
@@ -6,10 +6,10 @@ from application import app
 
 @app.route('/app4', methods=['POST'])
 def app4():
-  data1 = requests.get('http://app1:5000')
-  if data1.text == '22 April':
+  data1 = request.data.decode('utf-8')
+  if data1 == '22 April':
     output = "You share my birthday"
-  elif '22' == data1.text:
+  elif '22' == data1:
     output ='You share the same day'
   else:
     output = "Unlucky you do not share my birthday"
