5982eeb8eb4285e08461de381f28bd09b4e02b01
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-02 12:03:26 +0000

    fixes

--- app4/application/routes.py
+++ app4/application/routes.py
@@ -9,6 +9,8 @@ def app4():
   data1 = requests.get('http://app1:5000')
   if data1.text == '22 April':
     output = "You share my birthday"
+  elif 'April' in data1.text:
+    output ='You share the same month'
   else:
     output = "Unlucky you do not share my birthday"
   
