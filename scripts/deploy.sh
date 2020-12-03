3908bfccab533cd0fb111c7c3422373efde39e0d
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-03 16:09:35 +0000

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ docker pull ryanpurchase288/app4:latest
 git clone https://github.com/ryanpurchase288/week-9-project.git
 cd week-9-project
 docker stack deploy --compose-file docker-compose.yaml projectApp
-cp nignx.conf $(pwd)
+cp week-9-project/nignx.conf $(pwd)
 docker run -d -p 80:80 --name nginx-swarm-ingress --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
 EOF
