f8ac136301b868ac847058e8a0f18ae21e1c5af7
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-03 11:30:37 +0000

    ansible fixes

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -2,5 +2,5 @@
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 source ~/.bashrc
-pip3 install --user ansible
+#pip3 install --user ansible
 ansible-playbook -i inventory playbook.yaml
