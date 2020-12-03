a16a9757aae545e949da47a9768553de2cc0840d
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-03 12:33:51 +0000

    updated deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
-  
-#! /bin/bash
-
-docker stack deploy --compose-file docker-compose.yaml application
+  #!/bin/bash
+ssh manager << EOF
+scp /home/ryanpurchase288_rp/Week-9-project/docker-compose.yaml /home/jenkins/
+docker stack deploy --compose-file docker-compose.yaml sfia2
+EOF
