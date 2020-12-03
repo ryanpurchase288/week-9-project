51c6adcc1ce8d8dcb83bf6df0683b59f0836df4c
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-03 15:42:26 +0000

    error fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,6 @@ docker pull ryanpurchase288/app4:latest
 git clone https://github.com/ryanpurchase288/week-9-project.git
 cd week-9-project
 docker stack deploy --compose-file docker-compose.yaml projectApp
-docker run -d -p 80:80 --name nginx-swarm-ingress --mount type=bind,source=home/jenkins/week-9-project/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
+pwd
+docker run -d -p 80:80 --name nginx-swarm-ingress --mount type=bind,source=/home/jenkins/week-9-project/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
 EOF
