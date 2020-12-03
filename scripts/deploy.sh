340f6208ef259f61a323baa7c0b2b6b1a44405ef
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-03 15:55:08 +0000

    fixing path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,6 @@ docker pull ryanpurchase288/app4:latest
 git clone https://github.com/ryanpurchase288/week-9-project.git
 cd week-9-project
 docker stack deploy --compose-file docker-compose.yaml projectApp
-pwd
-cd ~/week-9-project
-docker run -d -p 80:80 --name nginx-swarm-ingress --mount type=bind,source=$(pwd)/week-9-project/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
+nginxPath=pwd
+docker run -d -p 80:80 --name nginx-swarm-ingress --mount type=bind,source=$(nginxPath)/week-9-project/nginx.conf,target=/etc/nginx/nginx.conf nginx:alpine
 EOF
