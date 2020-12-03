5814f5c036cb259489fd99d34282f4f8dacd4821
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-03 16:46:40 +0000

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ docker pull ryanpurchase288/app1:latest
 docker pull ryanpurchase288/app2:latest
 docker pull ryanpurchase288/app3:latest
 docker pull ryanpurchase288/app4:latest
-docker pull ryanpurchase288/nginx:alpine
+docker pull nginx:alpine
 git clone https://github.com/ryanpurchase288/week-9-project.git
 cd week-9-project
 docker stack deploy --compose-file docker-compose.yaml projectApp
