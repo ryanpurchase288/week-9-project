61a2191b892d5758eb7a21157146d17b15870f7f
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-03 12:58:50 +0000

    fixed error in deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
   #!/bin/bash
 ssh manager << EOF
-scp /home/ryanpurchase288_rp/week-9-project/docker-compose.yaml /home/jenkins/
+docker pull ryanpurchase288/app1:latest
+docker pull ryanpurchase288/app2:latest
+docker pull ryanpurchase288/app3:latest
+docker pull ryanpurchase288/app4:latest
+git clone https://github.com/ryanpurchase288/week-9-project.git
+cd week-9-project
 docker stack deploy --compose-file docker-compose.yaml sfia2
 EOF
