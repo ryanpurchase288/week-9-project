aae3110a6de61d79b76b54c0c0b63920f0495857
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-04 15:00:05 +0000

    attempt at database 6

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,6 @@ docker pull ryanpurchase288/app2:latest
 docker pull ryanpurchase288/app3:latest
 docker pull ryanpurchase288/app4:latest
 docker pull ryanpurchase288/database:latest
-docker pull nginx:alpine
 git clone https://github.com/ryanpurchase288/week-9-project.git
 cd week-9-project
 docker stack deploy --compose-file docker-compose.yaml projectApp
