6741cc4fc90bbb2656c2dd55d6c94a90238699a8
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-12-04 15:17:29 +0000

    database

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -2,5 +2,5 @@
 mkdir -p ~/.local/bin
 echo 'PATH=$PATH:home/jenkins/.local/bin' >> ~/.bashrc
 source ~/.bashrc
-pip3 install --user ansible
+#pip3 install --user ansible
 /home/jenkins/.local/bin/ansible-playbook -i inventory playbook.yaml
